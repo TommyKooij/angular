1ca4b7f59c4047ef7b2a48127f8e500f6283b0a3
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-10-14 11:30:38 +0200

    Added routing

--- homes-app/src/app/app.component.ts
+++ homes-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
@@ -14,11 +15,11 @@ import { HomeComponent } from "./home/home.component";
       />
     </header>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>`,
   styleUrls: ["./app.component.css"],
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
   title = "homes";
